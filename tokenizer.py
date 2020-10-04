f9a6872f694f0160796e730b4b9dd1fdedf58a00
Mahmood Rasuli <rasuli.ut@gmail.com>
2020-10-04 22:18:49 +0330

    initial

---
+++ tokenizer.py
@@ -0,0 +1,18 @@
+# convert a dictionary of clean descriptions to a list of descriptions
+def to_lines(descriptions):
+	all_desc = list()
+	for key in descriptions.keys():
+		[all_desc.append(d) for d in descriptions[key]]
+	return all_desc
+
+# fit a tokenizer given caption descriptions
+def create_tokenizer(descriptions):
+	lines = to_lines(descriptions)
+	tokenizer = Tokenizer()
+	tokenizer.fit_on_texts(lines)
+	return tokenizer
+
+# prepare tokenizer
+tokenizer = create_tokenizer(train_descriptions)
+vocab_size = len(tokenizer.word_index) + 1
+print('Vocabulary Size: %d' % vocab_size)
